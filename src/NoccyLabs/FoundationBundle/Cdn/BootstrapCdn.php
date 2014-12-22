02195587921e9175d6b5092fea50f8b77c508cd5
Christopher Vagnetoft <cvagnetoft@gmail.com>
2014-12-22 11:34:21 +0100

    Updated resource files

--- src/NoccyLabs/FoundationBundle/Cdn/BootstrapCdn.php
+++ src/NoccyLabs/FoundationBundle/Cdn/BootstrapCdn.php
@@ -8,61 +8,67 @@ class BootstrapCdn implements CdnInterface
 {
     public function update($path)
     {
-
+        $vers = "3.3.1";
         $libs = array(
             "twitter-bootstrap-css" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootstrap/{$vers}/css/bootstrap.min.css"
             ),
             "twitter-bootstrap" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"
+                "url" => "//maxcdn.bootstrapcdn.com/bootstrap/{$vers}/js/bootstrap.min.js"
             ),
             "font-awesome" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
             ),
             "bootswatch-amelia" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/amelia/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/amelia/bootstrap.min.css"
             ),
             "bootswatch-cerulean" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/cerulean/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/cerulean/bootstrap.min.css"
             ),
             "bootswatch-cosmo" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/cosmo/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/cosmo/bootstrap.min.css"
             ),
             "bootswatch-cyborg" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/cyborg/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/cyborg/bootstrap.min.css"
             ),
             "bootswatch-darkly" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/darkly/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/darkly/bootstrap.min.css"
             ),
             "bootswatch-flatly" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/flatly/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/flatly/bootstrap.min.css"
             ),
             "bootswatch-journal" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/journal/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/journal/bootstrap.min.css"
             ),
             "bootswatch-lumen" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/lumen/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/lumen/bootstrap.min.css"
+            ),
+            "bootswatch-paper" => array(
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/paper/bootstrap.min.css"
             ),
             "bootswatch-readable" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/readable/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/readable/bootstrap.min.css"
+            ),
+            "bootswatch-sandstone" => array(
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/sandstone/bootstrap.min.css"
             ),
             "bootswatch-simplex" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/simplex/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/simplex/bootstrap.min.css"
             ),
             "bootswatch-slate" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/slate/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/slate/bootstrap.min.css"
             ),
             "bootswatch-spacelab" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/spacelab/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/spacelab/bootstrap.min.css"
             ),
             "bootswatch-superhero" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/superhero/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/superhero/bootstrap.min.css"
             ),
             "bootswatch-united" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/united/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/united/bootstrap.min.css"
             ),
             "bootswatch-yeti" => array(
-                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/yeti/bootstrap.min.css"
+                "url" => "//maxcdn.bootstrapcdn.com/bootswatch/{$vers}/yeti/bootstrap.min.css"
             ),
         );
         
